e05c8978750504f34fb102c5e7bde4baf0357ed7
Jennifer11607 <jenniferjimenez152000@gmail.com>
2024-07-09 13:55:34 -0600

    creacion de ejemplos de dartos string

---
+++ script.js
@@ -0,0 +1,28 @@
+//Usar tres metodos por cada tipo de dato visto (string, number, bool, object, array). 
+
+//STRING
+
+//1. charAt 
+//Devuelve el caracter en la posicion especifica de una cadena 
+
+let str = "Hola";
+console.log(str.charAt(1)); // "j"
+
+//2. toUpperCase()
+//Convierte todos los caracteres de una cadena a mayúsculas.
+
+let stri = "hola";
+console.log(str.toUpperCase()); // "HOLA"
+
+//3. includes()
+//Comprueba si una cadena contiene otra cadena.
+
+let strin = "Hola mundo";
+console.log(str.includes("mundo")); // true
+
+
+//NUMBER
+
+//1. toFixed()
+// Formatea un número utilizando notación de punto fijo.
+
